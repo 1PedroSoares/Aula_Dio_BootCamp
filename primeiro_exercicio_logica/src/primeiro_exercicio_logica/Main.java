81e03b558de85bd7d4e800d91eab9299a30c148c
Pedro Henrique Soares <119024635+1PedroSoares@users.noreply.github.com>
2023-09-22 22:35:36 -0300

    Add files via upload

---
+++ primeiro_exercicio_logica/src/primeiro_exercicio_logica/Main.java
@@ -0,0 +1,40 @@
+package primeiro_exercicio_logica;
+
+import java.util.Scanner;
+public class Main {
+
+	public static void main (String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double raio , pi, area ;
+		
+			System.out.println("DIGITE O RAIO. EXEMPLO: 2,0:    ");
+		raio = sc.nextDouble();
+		pi = 3.14159f;
+		area = ((raio * raio) * pi);
+		
+			System.out.printf("A=%.4f%n" , area );
+		
+//	Fazer um programa para ler quatro valores inteiros A, B, C e D.
+//	A seguir, calcule e mostre a diferença do produto de A e B 
+//pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).	
+			
+			public static void Exercício3 (String[] args) {
+				
+				Scanner sc = new Scanner(System.in);	
+			
+		System.out.println("DIGITE O VALOR DE A:   ");
+		int a = sc.nextInt();
+		System.out.println("DIGITE O VALOR DE B:   ");
+		int b = sc.nextInt();
+		System.out.println("DIGITE O VALOR DE C:   ");
+		int c = sc.nextInt();	
+		System.out.println("DIGITE O VALOR DE D:   ");
+		int d = sc.nextInt();		
+		System.out.println("DIFERENÇA=");
+		
+		
+	}
+
+}
